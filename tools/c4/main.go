fc1172b445d391b2f4cc966932271dff6645fd48
Jerens Lensun <54782057+jerensl@users.noreply.github.com>
2022-04-04 13:41:42 +0800

    adding readme for better documentation

---
+++ tools/c4/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	service "github.com/jerensl/jerens-web-api/internal/service"
+	"github.com/krzysztofreczek/go-structurizr/pkg/scraper"
+	"github.com/krzysztofreczek/go-structurizr/pkg/view"
+)
+
+const (
+	scraperConfig = "scraper.yml"
+	viewConfig = "view.yml"
+	outputFile = "out/view-%s.plantuml"
+)
+
+func main() {
+	ctx := context.Background()
+	app := service.NewApplication(ctx)
+	scrape(app, "notification")
+}
+
+func scrape(app interface{}, name string) {
+	s, err := scraper.NewScraperFromConfigFile(scraperConfig)
+	if err != nil {
+		panic(err)
+	}
+
+	structure := s.Scrape(app)
+
+	outFileName := fmt.Sprintf(outputFile, name)
+	outFile, err := os.Create(outFileName)
+	if err != nil {
+		panic(err)
+	}
+
+	defer func() {
+		_ = outFile.Close()
+	}()
+
+	v, err := view.NewViewFromConfigFile(viewConfig)
+	if err != nil {
+		panic(err)
+	}
+
+	err = v.RenderStructureTo(structure, outFile)
+	if err != nil {
+		panic(err)
+	}
+}
